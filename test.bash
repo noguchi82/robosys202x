ac3a1335a943474962abc4fe591a1e913a1fcf9e
noguchi82 <s20C1093JP@s.chibakoudai.jp>
2022-11-25 18:46:46 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,26 +1,6 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
-# SPDX-License-Identifier: BSD-3-Clause
+#!/bin/bash
 
-ng () {
-		echo NG at Line $1
-			res=1
-}
-
-res=0
-
-### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus) 
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "${out}" = 14 ]
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK
-exit $res
